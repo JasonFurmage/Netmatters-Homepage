990a45be4849034cecf4b0d3ad52e1480915f2d8
JasonFurmage <150832588+JasonFurmage@users.noreply.github.com>
2024-01-25 16:51:24 +0000

    Added carousel and banner slides + some heading alignment fixes.

---
+++ js/app.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+    $('.carousel').slick({
+      autoplay: true,
+      autoplaySpeed: 5000,
+      speed: 275,
+      arrows : false,
+      dots: true,
+    });
+});
