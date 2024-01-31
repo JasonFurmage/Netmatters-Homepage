d63d0d82427170689605254aa9d0113152243e76
JasonFurmage <150832588+JasonFurmage@users.noreply.github.com>
2024-01-31 17:45:56 +0000

    Added cookies popup.

--- js/app.js
+++ js/app.js
@@ -1,3 +1,25 @@
+/* Cookies Popup
+   ========================================================================== */
+
+const cookiesOverlay = document.getElementById('js-cookies-overlay');
+const didSetCookies = localStorage.getItem("didSetCookies");
+
+// Show cookies popup if preference has not been set.
+if (didSetCookies === null ) {
+  showCookiesPopup();
+}
+
+// Set cookies preference and remove overlay.
+function setCookiesPreference () {
+  localStorage.setItem("didSetCookies", true);
+  cookiesOverlay.style.display = 'none';
+}
+
+function showCookiesPopup() {
+  cookiesOverlay.style.display = 'flex';
+}
+
+
 /* Slick Carousel Config
    ========================================================================== */
 
