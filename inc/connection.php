3bb33fdd914b97b987e56119ae1bd8889acaa73b
JasonFurmage <150832588+JasonFurmage@users.noreply.github.com>
2024-03-05 13:30:31 +0000

    Set up env variables.

--- inc/connection.php
+++ inc/connection.php
@@ -1,7 +1,16 @@
 <?php
 
+require_once realpath(__DIR__ . "/../vendor/autoload.php");
+use Dotenv\Dotenv;$dotenv = Dotenv::createImmutable(__DIR__ . '/../');
+$dotenv->load();
+
+$dbHost = $_ENV['MySQL_DB_HOST'];
+$dbUserName = $_ENV['MySQL_DB_USER_NAME'];
+$dbPassword = $_ENV['MySQL_DB_PASSWORD'];
+$dbName = $_ENV['MySQL_DB_NAME'];
+
 try {
-    $db = new PDO("mysql:host=localhost;dbname=database", "root", "");
+    $db = new PDO("mysql:host=$dbHost;dbname=$dbName", "$dbUserName", "$dbPassword");
     $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch (Exception $e) {
    echo "Unable to connect";
